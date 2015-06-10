5e899b4fbe416e94d4acc36fdbf4c9122007ebfb
asega <asega@komputer-unyu>
2015-06-10 17:28:17 +0700

    Merge origin/master
    Conflicts:
    	.gitignore

---
+++ backend/config/sahidmontana.php
@@ -0,0 +1,12 @@
+<?php
+//=========change this scope=========================================================
+$root = '/var/www/landa/sahidmontana_cp/backend/';
+$themesUrl = 'http://192.168.1.103/landa/sahidmontana_cp/backend/themes/';;
+$db = 'landa_cms_sahidmontana';
+$dbUser = 'root';
+$dbPwd = 'bismillah';
+$client = 'sahidmontana';
+$clientName = 'Sahid Montana';
+$rootUrl = 'http://192.168.1.103/landa/sahidmontana/backend/www/'.$client.'/';
+//===================================================================================
+?>
